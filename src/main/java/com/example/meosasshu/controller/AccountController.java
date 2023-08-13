7f516d2759e1b4e53e0cb352f26d3df4be44bbc6
Yeom <ydh7219@naver.com>
2023-08-13 18:29:30 +0900

    feature/22-account

--- src/main/java/com/example/meosasshu/controller/AccountController.java
+++ src/main/java/com/example/meosasshu/controller/AccountController.java
@@ -3,15 +3,15 @@ package com.example.meosasshu.controller;
 
 import com.example.meosasshu.common.security.user.CurrentUser;
 import com.example.meosasshu.common.security.user.UserDetailsImpl;
+import com.example.meosasshu.dto.request.SignupReqDTO;
+import com.example.meosasshu.dto.response.AccountResDTO;
 import com.example.meosasshu.dto.response.AddressResDTO;
 import com.example.meosasshu.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -23,6 +23,20 @@ public class AccountController {
     @GetMapping("/address")
     public ResponseEntity<AddressResDTO> getAddress(@AuthenticationPrincipal UserDetailsImpl userDetails){
         AddressResDTO dto = accountService.getAddress(userDetails.getAccount());
+        return ResponseEntity.ok(dto);
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/profile")
+    public ResponseEntity<AccountResDTO> getProfile(@CurrentUser UserDetailsImpl userDetails){
+        AccountResDTO dto = accountService.getProfile(userDetails.getAccount());
+        return ResponseEntity.ok(dto);
+    }
+
+    @Secured("ROLE_USER")
+    @PutMapping("/profile")
+    public ResponseEntity<AccountResDTO> updateProfile(@RequestBody SignupReqDTO reqDTO, @CurrentUser UserDetailsImpl userDetails){
+        AccountResDTO dto = accountService.updateProfile(reqDTO,userDetails.getAccount());
         return ResponseEntity.ok(dto);
     }
 
